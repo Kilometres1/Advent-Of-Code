ff938e9c1c728823b694dd0f665fe2e4895ea639
Kilometres1 <151871419+Kilometres1@users.noreply.github.com>
2025-12-12 18:28:42 +0000

    finished day 2

--- 2025/Day 5/part 2.py
+++ 2025/Day 5/part 2.py
@@ -1,39 +1,29 @@
-import re
 freshID = []#ids of fresh items
-availableIDs = []#ids of all available items
-finalIDs = []#the final fresh list
-IDbreak = 0
+interRange = [0,0]
 total = 0
 allIDs = []
-allIDs.append(0)
 file = open("Day 5/realData.txt","r")
 for line in file.read().split("\n"):
-    if line == "":#looking at the gap between them
-        break
-    else:#looking at the ranges
+    if line != "":
         ranges = line.split("-")
-        freshID.append(ranges)
-n = 0
-for i in freshID:
-    print(n)
-    #print("freshID:",i)
-    for j in range(int(i[0]),int(i[1])+1):
-        #print(j)
-        allIDs.sort()
-        for k in allIDs:
-            #print("checking",k)
-            if j == k:
-                break
-            elif k == allIDs[-1]:
-                allIDs.append(j)
-                #print("not found, adding",j)
-
-        
-    
-
-    allIDs = list(dict.fromkeys(allIDs))#remove duplicates
-    n += 1
-#print(allIDs)
-print(len(allIDs)-1)
-#print(freshID)
-#print(availableIDs)
+        interRange[0] = int(ranges[0])
+        interRange[1] = int(ranges[1])
+        freshID.append(interRange[:])
+    else:
+        break
+freshID.sort(key=lambda x: x[0])
+allIDs.append(freshID[0])
+freshID.pop(0)
+for j in freshID:
+    i = allIDs[-1]
+    if i == j:
+        continue
+    elif j[0] >= i[0] and j[1] <= i[1]:
+        continue
+    elif j[0] <= i[1] or j[0] == i[1]+1:
+        i[1] = j[1]
+    elif j[0] > i[1]+1:
+        allIDs.append(j)
+for i in allIDs:
+    total += (i[1] - i[0])+1
+print(total)
